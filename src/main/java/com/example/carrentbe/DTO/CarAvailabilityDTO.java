9c0abe1899cc32ff0ec17813aa092e8f27a9cdf6
ahmed <es-ahmedm.abdelwahed2025@alexu.edu.eg>
2024-01-02 11:48:54 +0200

    hena kol el reports  bas na2esha el search

--- src/main/java/com/example/carrentbe/DTO/CarAvailabilityDTO.java
+++ src/main/java/com/example/carrentbe/DTO/CarAvailabilityDTO.java
@@ -3,10 +3,16 @@ public class CarAvailabilityDTO{
     private String plateId;
     private boolean isAvailable;
     // Constructors, getters and setters
-
+    String reservationStatus;
     public CarAvailabilityDTO(String plateId, boolean isAvailable) {
         this.plateId = plateId;
         this.isAvailable = isAvailable;
+        if (isAvailable) {
+            this.reservationStatus = "Available";
+        } else {
+            this.reservationStatus = "Unavailable";
+        }
+
     }
 
     public String getPlateId() {
@@ -19,6 +25,14 @@ public class CarAvailabilityDTO{
 
     public boolean isAvailable() {
         return isAvailable;
+    }
+
+    public String getReservationStatus() {
+        return reservationStatus;
+    }
+
+    public void setReservationStatus(String reservationStatus) {
+        this.reservationStatus = reservationStatus;
     }
 
     public void setAvailable(boolean available) {
